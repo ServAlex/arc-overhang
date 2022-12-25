f065262707eb57a8cf958462c5698a5f42f181bd
Alex Servirog <servalex1@gmail.com>
2022-12-25 14:48:44 +0200

    removed original code that is not used, added gcode generation

--- main.py
+++ main.py
@@ -154,13 +154,13 @@ base_poly = util.create_rect(150, 20, 20, 30, True)
 # hard
 #base_poly = Polygon([(95.03307591266207, 61.44242332237353), (93.2611944512131, 54.03978522828941), (84.18173584271653, 50.71604934203349), (87.85468744487517, 42.61007177424688), (90.4103518773317, 40.55211002769791), (95.13321538102718, 42.227355495114075), (96.83477977873106, 38.14555774373097), (99.65886967537523, 38.98265402628894), (103.4901975249252, 40.25797485017517), (109.69626582291217, 42.55380147688191), (103.84477881018918, 48.603689008861444), (104.97768296404406, 50.63600468887961), (107.4394892600432, 56.047883366279436), (104.4248669124622, 58.060614733550565), (98.96258436494416, 57.162750031589866), (95.03307591266207, 61.44242332237353)])
 # failing
-base_poly = Polygon([(92.34956071425829, 54.98432201279906), (83.98922940950823, 50.298155643332066), (91.69906038241683, 46.18093404929046), (91.73874273139555, 41.28382241500534), (96.60078706451684, 44.13052535502262), (97.94144036974764, 42.778250547216), (99.63956634607004, 40.66419332670824), (101.25110955671346, 44.0506328126564), (109.35258711537831, 36.33079353221364), (110.47063467507401, 45.796212134023314), (109.8329206008391, 51.296329179873766), (109.47619943712579, 58.2113738963351), (103.44455712109193, 58.997805770454995), (97.18682148840615, 64.63316996934223), (94.94505227555427, 59.42156949681912), (92.34956071425829, 54.98432201279906)])
+#base_poly = Polygon([(92.34956071425829, 54.98432201279906), (83.98922940950823, 50.298155643332066), (91.69906038241683, 46.18093404929046), (91.73874273139555, 41.28382241500534), (96.60078706451684, 44.13052535502262), (97.94144036974764, 42.778250547216), (99.63956634607004, 40.66419332670824), (101.25110955671346, 44.0506328126564), (109.35258711537831, 36.33079353221364), (110.47063467507401, 45.796212134023314), (109.8329206008391, 51.296329179873766), (109.47619943712579, 58.2113738963351), (103.44455712109193, 58.997805770454995), (97.18682148840615, 64.63316996934223), (94.94505227555427, 59.42156949681912), (92.34956071425829, 54.98432201279906)])
 
 # Make the base polygon a randomly generated shape
-#base_poly = Polygon(util.generate_polygon(center=(x_axis, y_axis), avg_radius=avg_radius, irregularity=irregularity, spikiness=spikiness, num_vertices=num_vertices,))
+base_poly = Polygon(util.generate_polygon(center=(x_axis, y_axis), avg_radius=avg_radius, irregularity=irregularity, spikiness=spikiness, num_vertices=num_vertices,))
 
-skip_circles = False
-#skip_circles = True
+
+
 
 # Find starting edge (in this implementation, it just finds the largest edge to start from.
 # TODO Allow multiple starting points
@@ -224,74 +224,14 @@ curr_z -= LAYER_HEIGHT*2
 with open(OUTPUT_FILE_NAME, 'a') as gcode_file:
         gcode_file.write(f"G1 Z{'{0:.3f}'.format(curr_z)} F500\n")
 
-if not skip_circles:
-    # Create multiple layers
-    r = LINE_WIDTH
-    small_arc_radius = 0.5 # Arcs smaller than this get reduced speed and/or flow settings.
-    curr_arc = starting_arc
 
-    # Overlap arc with the starting line
-    starting_point = util.move_toward_point(starting_point, affinity.rotate(p1, 90, LineString([p1, p2]).centroid), LINE_WIDTH*0.5) 
 
-    # Create arcs until we reach the edge of the polygon
-    while r < r_start-THRESHOLD:
-
-        # Create a circle based on point location, radius, n
-        next_circle = Polygon(util.create_circle(starting_point.x, starting_point.y, r, N))
-        next_circle = affinity.rotate(next_circle, starting_line_angle, origin = 'centroid', use_radians=True)
-
-        # Plot arc
-        next_arc = util.create_arc(next_circle, base_poly, ax, depth=0)
-        if not next_arc:
-            r += LINE_WIDTH
-            continue
-        curr_arc = Polygon(next_arc)
-
-        #Slow down and reduce flow for all small arcs
-        if r < small_arc_radius:
-            speed_modifier = 0.25
-            e_modifier = 0.25
-        else: 
-            speed_modifier = 1
-            e_modifier = 1
-
-        # Write gcode to file
-        util.write_gcode(OUTPUT_FILE_NAME, next_arc, LINE_WIDTH, LAYER_HEIGHT, FILAMENT_DIAMETER, ARC_E_MULTIPLIER*e_modifier, FEEDRATE*speed_modifier, close_loop=False)
-        
-        r += LINE_WIDTH
-        
-        # Create image
-        #file_name = util.image_number(image_name_list)   
-        #plt.savefig(file_name, dpi=200)
-        #image_name_list.append(file_name + ".png")
-
-    remaining_empty_space = base_poly.difference(curr_arc)
-    next_point, longest_distance, _ = util.get_farthest_point(curr_arc, boundary_line, base_poly)
-
-    # If there's room for an arc to be built on top of the current arc, then do it!
-    while longest_distance > THRESHOLD + MIN_ARCS*LINE_WIDTH: 
-        next_arc, remaining_empty_space, image_name_list = util.arc_overhang(curr_arc, boundary_line, starting_line_angle, N, 
-                                                                            remaining_empty_space, next_circle, 
-                                                                            THRESHOLD, ax, fig, 1, image_name_list, 
-                                                                            R_MAX, MIN_ARCS, LINE_WIDTH, OUTPUT_FILE_NAME,
-                                                                            LAYER_HEIGHT, FILAMENT_DIAMETER, ARC_E_MULTIPLIER,
-                                                                            FEEDRATE)
-        next_point, longest_distance, _ = util.get_farthest_point(curr_arc, boundary_line, remaining_empty_space)
-
-# interpolation
+# new approach
 
 shape_target = boundary_line.simplify(0)
 
-if(skip_circles):
-    remaining_empty = base_poly
-    current_line = MultiLineString([starting_line])
-else:
-    shape_completed = remaining_empty_space.exterior.difference(boundary_line)
-    if shape_completed.geom_type == 'MultiLineString':
-        shape_completed = ops.linemerge(shape_completed)
-
-    remaining_empty = remaining_empty_space
-    current_line = MultiLineString([shape_completed])
+remaining_empty = base_poly
+current_line = MultiLineString([starting_line])
 
 gpd.GeoSeries(shape_target).plot(ax=ax[0], color='magenta', linewidth=1)
 gpd.GeoSeries(current_line).plot(ax=ax[0], color='lime', linewidth=1)
@@ -319,38 +259,14 @@ while remaining_empty.area > 0:
 
     gpd.GeoSeries(current_line).plot(ax=ax[0], color='blue', linewidth=1)
 
+    if current_line.geom_type == "MultiLineString":
+        [util.write_gcode(OUTPUT_FILE_NAME, geom, LINE_WIDTH, LAYER_HEIGHT, FILAMENT_DIAMETER, ARC_E_MULTIPLIER, FEEDRATE, close_loop=False) for geom in current_line.geoms]
+    else:
+        if current_line.length > 0.1:
+            util.write_gcode(OUTPUT_FILE_NAME, current_line, LINE_WIDTH, LAYER_HEIGHT, FILAMENT_DIAMETER, ARC_E_MULTIPLIER, FEEDRATE, close_loop=False)
+
     #while not plt.waitforbuttonpress(): pass
 
-
-
-# Add concentric rings around the outside of the perimeter
-# TODO don't use a for loop.....
-# TODO write interpolation here
-"""
-for i in range(100):
-    first_ring = LineString(Polygon(boundary_line).buffer(-99*LINE_WIDTH + LINE_WIDTH*i).exterior.coords)
-    first_ring = first_ring.intersection(remaining_empty_space)
-    if first_ring.length <1e-9:
-        continue
-
-    if first_ring.geom_type == 'LineString':
-        line = first_ring
-        # plot starting line
-        first_ring_geoseries = gpd.GeoSeries(line)
-        first_ring_geoseries.plot(ax=ax[0], color='blue', edgecolor = 'blue', linewidth=1)
-        util.write_gcode(OUTPUT_FILE_NAME, line, LINE_WIDTH, LAYER_HEIGHT, FILAMENT_DIAMETER, ARC_E_MULTIPLIER, FEEDRATE, False)
-    else:
-        for line in first_ring.geoms:
-            # plot starting line
-            first_ring_geoseries = gpd.GeoSeries(line)
-            first_ring_geoseries.plot(ax=ax[0], color='blue', edgecolor = 'blue', linewidth=1)
-            util.write_gcode(OUTPUT_FILE_NAME, line, LINE_WIDTH, LAYER_HEIGHT, FILAMENT_DIAMETER, ARC_E_MULTIPLIER, FEEDRATE, False)
-
-            #Make image
-            #file_name = util.image_number(image_name_list)   
-            #plt.savefig(file_name, dpi=200)
-            #image_name_list.append(file_name + ".png")
-"""
 
 """
 # Turn images into gif + MP4
